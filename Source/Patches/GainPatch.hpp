b7da03eb36f366dcf4c6b602edeef7794a255ff6
mars <martin@rebel-it.com>
2013-06-21 18:15:44 +0100

    simplified buffer handling

--- Source/Patches/GainPatch.hpp
+++ Source/Patches/GainPatch.hpp
@@ -13,12 +13,12 @@ public:
     registerParameter(PARAMETER_E, "E");
   }
   void processAudio(AudioInputBuffer &input, AudioOutputBuffer &output){
-    int size = input.getSize();
     float gain = getParameterValue(PARAMETER_D);
-    float* buf = input.getSamples();
+    float* x = input.getSamples();
+    float* y = output.getSamples();
+    int size = input.getSize();
     for(int i=0; i<size; ++i)
-      buf[i] = gain*buf[i];
-    output.setSamples(buf);
+      y[i] = gain*x[i];
   }
 };
 
